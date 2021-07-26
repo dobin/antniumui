f8604f339cb1087540be86fada3ca1f6ce1da096
Dobin <dobin@broken.ch>
2021-07-27 00:34:38 +0200

    feature: more reliable retry

--- src/app/admin-websocket.service.ts
+++ src/app/admin-websocket.service.ts
@@ -2,6 +2,7 @@ import { Injectable, EventEmitter } from '@angular/core';
 import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
 import { HttpErrorResponse } from '@angular/common/http';
 import { interval, Subscription } from 'rxjs';
+import { map, retry, catchError, delay } from 'rxjs/operators';
 
 import { PacketInfo, Packet, ClientInfo, Campaign } from './app.model';
 import { ApiService } from './api.service';
@@ -33,8 +34,85 @@ export class AdminWebsocketService {
     private apiService: ApiService,
     private configService: ConfigService,
   ) { 
+    // Client refresh function. Runs forever
+    this.refreshClients();
+    const source = interval(10000);
+    this.subscription = source.subscribe(val => this.refreshClients());
+
     this.connect();
   }
+
+  private connect() {
+    // Get initial data
+    this.apiService.refreshPackets()
+      .pipe(
+        retry(30), 
+        delay(1000) 
+      )
+      .subscribe(
+        (data: PacketInfo[]) => { 
+          this.packetInfos = data;
+          this.packetInfosEvent.emit(undefined); // undefined is broadcast all
+          this.connectWs();
+        },
+        (err: HttpErrorResponse) => {
+          this.restStatus = "error";
+        },
+    );
+  }
+
+  private connectWs() {
+    console.log("ConnectWS");
+
+    // Make WS, connect
+    if (!this.socket$ || this.socket$.closed) {
+    } else {
+    }
+
+      var newUrl = this.configService.getServerIp().replace('http', 'ws') + "/ws";
+      this.socket$ = webSocket({
+        url: newUrl,
+        openObserver: {
+          next: () => {
+            console.log("Websocket open");
+            this.websocketStatus = "Open";
+          },
+        },
+        closeObserver: {
+          next: () => {
+            this.websocketStatus = "Closed";
+            console.log('[WebSocket]: connection closed, retrying');
+            timer(0)
+            .pipe(take(1))
+            .subscribe(() => {
+              this.connect();
+            });
+            
+          }
+        },
+      });
+
+      // Send our authentication token
+      this.socket$.next(this.configService.getAdminApiKey());
+
+
+    // Function to listen for updates from WS
+    this.socket$.subscribe((data: WebsocketData) => {
+      var x: PacketInfo = this.packetInfos.find(packetInfo => packetInfo.Packet.packetid == data.PacketInfo.Packet.packetid ) as PacketInfo
+      if (x == undefined) {
+        // Add Packet
+        this.packetInfos.push(data.PacketInfo);
+      } else {
+        // Update Packet 
+        var index = this.packetInfos.indexOf(x);
+        Object.assign(this.packetInfos[index], data.PacketInfo);
+      }
+
+      // Notify
+      this.packetInfosEvent.emit(data.PacketInfo);
+     });
+  }
+
 
   public getPacketInfos(): PacketInfo[] {
     return this.packetInfos;
@@ -61,68 +139,6 @@ export class AdminWebsocketService {
     );
   }
 
-  private connect() {
-    // Get initial data
-    this.apiService.refreshPackets().subscribe(
-      (data: PacketInfo[]) => { 
-        this.packetInfos = data;
-        this.packetInfosEvent.emit(undefined); // undefined is broadcast all
-      },
-      (err: HttpErrorResponse) => {
-        console.log("HTTP Error: " + err);
-        this.restStatus = "error";
-      },
-    );
-
-    this.refreshClients();
-    const source = interval(10000);
-    this.subscription = source.subscribe(val => this.refreshClients());
-
-    // Make WS, connect
-    if (!this.socket$ || this.socket$.closed) {
-      var newUrl = this.configService.getServerIp().replace('http', 'ws') + "/ws";
-      this.socket$ = webSocket({
-        url: newUrl,
-        openObserver: {
-          next: () => {
-            this.websocketStatus = "Open";
-          },
-        },
-        closeObserver: {
-          next: () => {
-            this.websocketStatus = "Closed";
-            console.log('[WebSocket]: connection closed, retrying');
-            timer(0)
-            .pipe(take(1))
-            .subscribe(() => {
-              this.connect();
-            });
-            
-          }
-        },
-      });
-
-      // Send our authentication token
-      this.socket$.next(this.configService.getAdminApiKey());
-    }
-
-    // Function to listen for updates from WS
-    this.socket$.subscribe((data: WebsocketData) => {
-      var x: PacketInfo = this.packetInfos.find(packetInfo => packetInfo.Packet.packetid == data.PacketInfo.Packet.packetid ) as PacketInfo
-      if (x == undefined) {
-        // Add Packet
-
-        this.packetInfos.push(data.PacketInfo);
-      } else {
-        // Update Packet (by removing the previous one)
-        var index = this.packetInfos.indexOf(x);
-        Object.assign(this.packetInfos[index], data.PacketInfo);
-      }
-
-      // Notify
-      this.packetInfosEvent.emit(data.PacketInfo);
-     });
-  }
 
   disconnect() {
     this.socket$.complete();
