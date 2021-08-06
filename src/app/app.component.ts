aec79243417a20d2cd4dd55100cb60ef88e8fddd
Dobin <dobin@broken.ch>
2021-08-07 00:13:33 +0200

    show: server ip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { ConfigModalComponent } from './config-modal/config-modal.component';
 import { MatDialog } from "@angular/material/dialog";
 import { AdminWebsocketService } from './admin-websocket.service';
 import { environment } from '../environments/environment';
+import { ConfigService } from './config.service';
 
 @Component({
   selector: 'app-root',
@@ -13,10 +14,14 @@ export class AppComponent {
   title = 'antniumui';
   isProd: boolean = false;
 
+  serverIp: string = "";
+
   constructor(
-    private adminWebsocketService: AdminWebsocketService, // This connects the WS via init()
+    //private adminWebsocketService: AdminWebsocketService, // This connects the WS via init()
     private dialog: MatDialog,
+    private configService: ConfigService,
   ) {
+    this.serverIp = configService.getServerIp();
     if (environment.production) {
       this.isProd = true;
     } else {
